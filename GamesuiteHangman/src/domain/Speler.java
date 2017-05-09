9f828296d2fafe5388359458113713916058d164
RobbeCuyvers <robbe.cuyvers@student.ucll.be>
2017-05-09 10:45:33 +0200

    Domain + Speler

---
+++ GamesuiteHangman/src/domain/Speler.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Speler {
+	private String naam;
+	private int score;
+	
+	
+	public String getNaam() {
+		return naam;
+	}
+	private void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public int getScore() {
+		return score;
+	}
+	private void setScore(int score) {
+		this.score = score;
+	}
+	
+
+}

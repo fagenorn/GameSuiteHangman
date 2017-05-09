6512e1a0a131dbed3899faa00a05c2d99bbf66d7
Daan <daan.adams@student.ucll.be>
2017-05-09 11:32:04 +0200

    Edit UI to show point coordinates - Daan

--- GamesuiteHangman/src/ui/UI.java
+++ GamesuiteHangman/src/ui/UI.java
@@ -1,7 +1,29 @@
 package ui;
 
+import javax.swing.JOptionPane;
+
+import domain.Speler;
+import model.Punt;
+
 public class UI {
 	
-	public void load();
+	private final Speler speler;
+	
+	public UI(Speler s){
+		speler = s;
+	}
+	
+	public void load(){
+		String xString = JOptionPane.showInputDialog("x coordinaat van het punt");
+		String yString = JOptionPane.showInputDialog("y coordinaat van het punt");
+		try{
+			int x = Integer.parseInt(xString);
+			int y = Integer.parseInt(yString);
+			Punt p = new Punt(x, y);
+			JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: ("+p.getX() +", " + p.getY()+")");
+		} catch(NumberFormatException e){
+			JOptionPane.showMessageDialog(null, "Dit is geen correct punt.");
+		}
+	}
 
 }

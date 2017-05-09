6221a0a38256b70d7b7a61552a407a0bcc4a08ab
fagenorn <anisanissakkaf@gmail.com>
2017-05-09 15:54:34 +0200

    Auto stash before merge of "master" and "origin/master"
    Zorg dat vorm binnen tekening is.

--- GamesuiteHangman/src/model/Tekening.java
+++ GamesuiteHangman/src/model/Tekening.java
@@ -4,55 +4,63 @@ import java.util.ArrayList;
 
 import domain.DomainException;
 
-public class Tekening{
+public class Tekening {
 	private String naam;
 	private static final int MIN_X = 0;
 	private static final int MIN_Y = 0;
 	private static final int MAX_X = 399;
 	private static final int MAX_Y = 399;
 	private ArrayList<Vorm> vormen = new ArrayList<>();
-	
-	
+
 	public String getNaam() {
 		return naam;
 	}
+
 	private void setNaam(String naam) {
-		if (naam == null || naam.trim().isEmpty()){
+		if (naam == null || naam.trim().isEmpty()) {
 			throw new DomainException("");
 		}
 		this.naam = naam;
 	}
-	
-	public Tekening(String naam){
+
+	public Tekening(String naam) {
 		setNaam(naam);
-		
+
 	}
-	
-	public void voegToe(Vorm vorm){
-		if (vorm == null || vormen.contains(vorm)){
+
+	public void voegToe(Vorm vorm) {
+		if (vorm == null || vormen.contains(vorm)) {
 			throw new DomainException("");
 		}
-		vormen.add(vorm);
+
+		if (vorm.getOmhullende().getMinimumX() >= MIN_X && vorm.getOmhullende().getMaximumX() <= MAX_X
+				&& vorm.getOmhullende().getMinimumY() >= MIN_Y && vorm.getOmhullende().getMaximumY() >= MAX_Y)
+
+			vormen.add(vorm);
 	}
-	public Vorm getVorm(int index){
+
+	public Vorm getVorm(int index) {
 		return null;
 	}
-	public int getAantalVormen(){
+
+	public int getAantalVormen() {
 		return 0;
 	}
-	public void verwijder(Vorm vorm){
-		
+
+	public void verwijder(Vorm vorm) {
+
 	}
-	public boolean bevat(Vorm vorm){
+
+	public boolean bevat(Vorm vorm) {
 		return false;
 	}
-	public String toString(){
+
+	public String toString() {
 		return "";
 	}
-	public boolean equals(Object o){
+
+	public boolean equals(Object o) {
 		return false;
 	}
-	
-	
 
 }

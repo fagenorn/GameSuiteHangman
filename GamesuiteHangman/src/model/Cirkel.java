2d70fa75ef825c9181635416dd8284c573f0dd21
RobbenP <robben.praet@gmail.com>
2017-05-09 13:04:54 +0200

    bugfix

--- GamesuiteHangman/src/model/Cirkel.java
+++ GamesuiteHangman/src/model/Cirkel.java
@@ -5,6 +5,8 @@ public class Cirkel {
 	private int radius;
 	private Punt middelpunt;
 	public Cirkel(Punt punt, int i) {
+		setRadius(i);
+		setMiddelpunt(punt);
 		
 	}
 	public int getRadius() {
@@ -25,7 +27,7 @@ public class Cirkel {
 	}
 	
 	public String toString(){
-		return "Cirkel: middelPunt: "+ getMiddelpunt().toString()+" - straal: " +getRadius();
+		return "Cirkel: middelPunt: "+ getMiddelpunt()+" - straal: " +getRadius();
 	}
 	
 	
--- GamesuiteHangman/src/ui/UI.java
+++ GamesuiteHangman/src/ui/UI.java
@@ -3,6 +3,7 @@ package ui;
 import javax.swing.JOptionPane;
 
 import domain.Speler;
+import model.Cirkel;
 import model.Punt;
 
 public class UI {
@@ -14,16 +15,38 @@ public class UI {
 	}
 	
 	public void load(){
+		cirkel(punt());
+	}
+
+	private Punt punt() {
+		do{
 		String xString = JOptionPane.showInputDialog("x coordinaat van het punt");
 		String yString = JOptionPane.showInputDialog("y coordinaat van het punt");
+		
 		try{
 			int x = Integer.parseInt(xString);
 			int y = Integer.parseInt(yString);
 			Punt p = new Punt(x, y);
 			JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: ("+p.getX() +", " + p.getY()+")");
+			return p;
 		} catch(NumberFormatException e){
 			JOptionPane.showMessageDialog(null, "Dit is geen correct punt.");
 		}
+		}while (true);
+		
+		
+	}
+	
+	private void cirkel(Punt p){
+		try{
+			int radius = Integer.parseInt(JOptionPane.showInputDialog("Geef de straal van de cirkel"));
+			Cirkel cirkel = new Cirkel(p,radius);
+			JOptionPane.showMessageDialog(null,cirkel.toString());
+		} catch (NumberFormatException e){
+			JOptionPane.showMessageDialog(null, "Dit is geen correte straal."); 
+		
+		
 	}
 
-}
+}}
+

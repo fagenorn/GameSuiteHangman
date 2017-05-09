2260df7da50adbc2683178b024ea77e20f7c7306
Daan <daan.adams@student.ucll.be>
2017-05-09 11:50:56 +0200

    Added Rechthoek and edited RechthoekTest - Daan

---
+++ GamesuiteHangman/src/model/Rechthoek.java
@@ -0,0 +1,58 @@
+package model;
+
+import domain.DomainException;
+
+public class Rechthoek {
+	
+	private Punt linkerboven;
+	private int breedte;
+	private int lengte;
+	
+	public Rechthoek(Punt p, int b, int l){
+		setLinkerBovenhoek(p);
+		setBreedte(b);
+		setHoogte(l);
+	}
+
+	private void setHoogte(int l) {
+		if(l <= 0){
+			throw new DomainException("Hoogte mag niet 0 zijn.");
+		}
+		lengte = l;
+	}
+
+	private void setBreedte(int b) {
+		if(b <= 0){
+			throw new DomainException("Breedte mag niet 0 zijn.");
+		}
+		breedte = b;
+	}
+
+	private void setLinkerBovenhoek(Punt p) {
+		if(!(p instanceof Punt)){
+			throw new DomainException("Punt mag niet null zijn.");
+		}
+		linkerboven = p;
+	}
+
+	public int getHoogte() {
+		return lengte;
+	}
+
+	public int getBreedte() {
+		return breedte;
+	}
+
+	public Punt getLinkerBovenhoek() {
+		return linkerboven;
+	}
+	
+	public boolean equals(Object o){
+		if(o instanceof Rechthoek){
+			Rechthoek r = (Rechthoek) o;
+			return getLinkerBovenhoek().equals(r.getLinkerBovenhoek()) && getHoogte() == r.getHoogte() && getBreedte() == r.getBreedte();
+		}
+		return false;
+	}
+
+}

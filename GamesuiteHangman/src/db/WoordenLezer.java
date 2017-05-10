d62ae9edffd24aaa4a3033ad42d5480a30b72249
RobbeCuyvers <robbe.cuyvers@student.ucll.be>
2017-05-10 11:48:43 +0200

    Story 13 - WoordenLezer + File

--- GamesuiteHangman/src/db/WoordenLezer.java
+++ GamesuiteHangman/src/db/WoordenLezer.java
@@ -1,31 +1,46 @@
 package db;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
 import model.WoordenLijst;
 
 public class WoordenLezer {
+	private File file;
 	
 	public WoordenLezer(String bestandsnaam){
-		
+		setBestandsNaam(bestandsnaam);
 	}
 	
-	public WoordenLijst lees(){
-		WoordenLijst content = null;
+	private void setBestandsNaam(String bestandsnaam) {
+		if(bestandsnaam == null || bestandsnaam.trim().isEmpty() /*|| bestandsnaam.substring(bestandsnaam.length()-5, bestandsnaam.length()-1) != ".txt"*/){
+			throw new DbException("");
+		}
+		this.file = new File(bestandsnaam);
+	}
 
-		try(BufferedReader br = new BufferedReader(new FileReader("hangman.txt"))) {
-		    for(String line; (line = br.readLine()) != null; ) {
-		    	content.voegToe(line);
-		    }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+	public WoordenLijst lees(){
+		WoordenLijst content = new WoordenLijst();
+		try {
+			Scanner sc = new Scanner(getFile());
+			while (sc.hasNextLine()){
+				String line = sc.nextLine();
+				content.voegToe(line);				
+			}
+			sc.close();
+		}catch (FileNotFoundException e){
 			e.printStackTrace();
 		}
 		return content;
+	}
+
+	public File getFile() {
+		return file;
 	}
 	
 

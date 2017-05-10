b566f3ef9d069ec872975c46a86adc7f497b01b0
MSI\bbuys <bjorn.buys@student.ucll.be>
2017-05-10 09:41:26 +0200

    Bjorn-Robbe - Story 08 - TekeningTest

--- GamesuiteHangman/test/domain/TekeningTest.java
+++ GamesuiteHangman/test/domain/TekeningTest.java
@@ -30,6 +30,13 @@ public class TekeningTest {
 		raambalk2 = new LijnStuk(new Punt(250, 220), new Punt(250, 280));
 		schouwNietInTekening = new Rechthoek(new Punt(150, 150), 20, 40);
 	}
+	
+	@Test
+	public void Kan_Vorm_Toevoegen(){
+		Tekening huis1 = new Tekening("huis1");
+		huis1.voegToe(dak);
+		assertTrue(huis1.bevat(dak));
+	}
 
 	@Test
 	public void Tekening_moet_een_tekening_aanmaken_met_een_geldige_naam_en_0_vormen() {

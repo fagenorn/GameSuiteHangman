0d8e6b2da0439e3d905ae15f7635bbff2e205e30
MSI\bbuys <bjorn.buys@student.ucll.be>
2017-05-10 10:22:45 +0200

    Bjorn, Robbe - Story 13 - WoordenLijst - Methodes implementeren uml

--- GamesuiteHangman/src/model/WoordenLijst.java
+++ GamesuiteHangman/src/model/WoordenLijst.java
@@ -2,10 +2,34 @@ package model;
 
 import java.util.ArrayList;
 
-public class WoordenLijst {
+import domain.DomainException;
 
+public class WoordenLijst {
+	private ArrayList<String> woorden = new ArrayList<>();
+	
 	public WoordenLijst() {
 
 	}
 
+	public int getAantalWoorden(){
+		return woorden.size();
+		
+	}
+	
+	public void voegToe(String woord){
+		if(woord == null || woord.trim().isEmpty() || woorden.contains(woord)){
+			throw new DomainException("Woord mag niet leeg zijn !");
+		}
+		woorden.add(woord);
+	}
+	
+	public String getRandomWoord(){
+		int randomG = (int) Math.floor(Math.random() * woorden.size());
+		if(woorden.size() == 0){
+			return null;
+		}
+	return woorden.get(randomG);
+		
+		
+	}
 }

8cf373325220bdf6ae20033ca68f8cb40485e949
fagenorn <anisanissakkaf@gmail.com>
2017-05-11 14:14:07 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ GamesuiteHangman/src/ui/GameHoofdScherm.java
@@ -0,0 +1,11 @@
+package ui;
+
+import model.TekeningHangMan;
+
+public class GameHoofdScherm {
+
+	public GameHoofdScherm(String naam, TekeningHangMan tekening) {
+		// TODO Auto-generated constructor stub
+	}
+
+}

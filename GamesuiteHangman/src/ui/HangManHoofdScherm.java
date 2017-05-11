a6e366d1fbda6cef941b05b74fee86575bcbfd34
fagenorn <anisanissakkaf@gmail.com>
2017-05-11 15:07:37 +0200

    Game is af.....

--- GamesuiteHangman/src/ui/HangManHoofdScherm.java
+++ GamesuiteHangman/src/ui/HangManHoofdScherm.java
@@ -4,36 +4,18 @@ import java.awt.BorderLayout;
 
 import model.HangMan;
 
-public class HangManHoofdScherm extends GameHoofdScherm {
+public class HangManHoofdScherm extends GameMainWindow {
 
 	private static final long serialVersionUID = 1L;
 
-	public HangManHoofdScherm(HangMan spel, HangmanPaneel spelpaneel) {
+	public HangManHoofdScherm(HangMan spel, HangmanPaneel spelpaneel) throws UIException {
 		super("Hangman - " + spel.getSpeler().getNaam(), spel.getTekening());
 
 		spelpaneel.setTekenVenster(getTekenvenster());
 		this.add(spelpaneel, BorderLayout.SOUTH);
 	}
-
-	private void add(HangmanPaneel spelpaneel, String south) {
-	}
-
-	private TekenVenster getTekenvenster() {
-		return null;
-	}
-
 	public void start() {
 		this.pack();
 		this.setVisible(true);
-	}
-
-	private void setVisible(boolean b) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void pack() {
-		// TODO Auto-generated method stub
-
 	}
 }
--- GamesuiteHangman/src/ui/HangmanPaneel.java
+++ GamesuiteHangman/src/ui/HangmanPaneel.java
@@ -11,84 +11,103 @@ import javax.swing.JTextField;
 
 import model.HangMan;
 
-
 public class HangmanPaneel extends JPanel {
 
-	private static final long serialVersionUID = 1L;	
-	
-	private JTextField letter;	
+	private static final long serialVersionUID = 1L;
+
+	private JTextField letter;
 	private JLabel woord;
-	
+
 	private TekenVenster tekenVenster;
 	private HangMan spel;
-	
-	public HangmanPaneel(HangMan spel){
+
+	public HangmanPaneel(HangMan spel) {
 		super();
 		setSpel(spel);
 		init();
 	}
 
-	private void init(){
-		letter = new JTextField("",5);
+	private void init() {
+		letter = new JTextField("", 5);
 		woord = new JLabel("");
-		
+
 		this.setLayout(new BorderLayout());
 		this.add(letter, BorderLayout.EAST);
 		this.add(woord, BorderLayout.CENTER);
-		
+
 		letter.addKeyListener(new RaadLuisteraar());
 	}
-	
+
 	private void reset() {
 		woord.setText(getSpel().getHint());
 		getTekenVenster().teken();
 	}
-	
+
 	public class RaadLuisteraar implements KeyListener {
 
 		@Override
 		public void keyPressed(KeyEvent arg0) {
-			if(arg0.getKeyCode()== KeyEvent.VK_ENTER){
+			if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
 				String input = letter.getText();
 				char guess = '\u0000';
-				if(input.length() > 0){
+				if (input.length() > 0) {
 					guess = input.charAt(0);
 				}
-				//TODO raad
+
+				spel.raad(guess);
 
 				woord.setText(getSpel().getHint());
 				letter.setText("");
 				getTekenVenster().teken();
-				
-				//TODO
-				//toon boodschap als gewonnen of verloren en vraag of speler opnieuw wilt spelen
-				//als de speler opnieuw wilt spelen: herzet het spel en het paneel
-				//anders stop (System.exit(0))
+
+				if (spel.isGewonnen()) {
+					String text = (String) JOptionPane.showInputDialog(null, "Geonnen!\nNog eens? (y/n)");
+					shouldExit(text);
+
+				} else if (spel.isGameOver()) {
+					String text = (String) JOptionPane.showInputDialog(null, "Verloren!\nNog eens? (y/n)");
+					shouldExit(text);
+				}
 			}
 		}
 
 		@Override
-		public void keyReleased(KeyEvent arg0) {/* Niet nodig*/}
+		public void keyReleased(KeyEvent arg0) {
+			/* Niet nodig */}
+
 		@Override
-		public void keyTyped(KeyEvent arg0) {/* Niet nodig*/}
+		public void keyTyped(KeyEvent arg0) {
+			/* Niet nodig */}
 	}
-	
-	private void setSpel(HangMan spel){
+
+	private void shouldExit(String text){
+		if (text.length() == 0) {
+			text = "n";
+		}
+		
+		if (text.charAt(0) == 'y') {
+			System.out.println(text);
+			spel.reset();
+			tekenVenster.repaint();
+		} else {
+			System.exit(0);
+		}
+		System.out.println(text);
+	}
+	private void setSpel(HangMan spel) {
 		this.spel = spel;
 	}
 
 	private HangMan getSpel() {
 		return spel;
 	}
-	
 
 	private TekenVenster getTekenVenster() {
 		return tekenVenster;
 	}
 
-	 public void setTekenVenster(TekenVenster tekenVenster) {
+	public void setTekenVenster(TekenVenster tekenVenster) {
 		this.tekenVenster = tekenVenster;
-
 		reset();
 	}
 }
